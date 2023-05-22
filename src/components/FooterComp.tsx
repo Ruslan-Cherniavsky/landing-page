0a42799d9838cfb013a445bb0d8370de218f782f
Ruslan Cherniavsky <ruslan@railvision.io>
2023-05-22 15:03:31 +0300

    new555

--- src/components/FooterComp.tsx
+++ src/components/FooterComp.tsx
@@ -6,7 +6,7 @@ function Footer() {
                 <div className="footer-copyright">
                     <div className="container">
                         © {new Date().getFullYear()} By Ruslan Cherniavsky
-                        <a className="grey-text text-lighten-4 right" href="#!">
+                        <a className="grey-text text-lighten-4 right" href="https://github.com/Ruslan-Cherniavsky/landing-page">
                             Repo
                         </a>
                     </div>
